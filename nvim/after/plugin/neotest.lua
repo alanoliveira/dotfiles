42233e8c8cf37d0abad6f95101093e2d39dab14b
Alan Oliveira <alan.oliveira89@gmail.com>
2022-10-04 15:25:22 +0900

    Fix neotest commands

--- nvim/after/plugin/neotest.lua
+++ nvim/after/plugin/neotest.lua
@@ -9,5 +9,5 @@ neotest.setup({
 vim.keymap.set("n", "<space>tn", function() neotest.run.run() end, { desc = "[test] run nearest" })
 vim.keymap.set("n", "<space>tf", function() neotest.run.run(vim.fn.expand("%")) end, { desc = "[test] run file" })
 vim.keymap.set("n", "<space>tl", function() neotest.run.run_last() end, { desc = "[test] run last" })
-vim.keymap.set("n", "<space>to", function() neotest.output.show() end, { desc = "[test] show output" })
-vim.keymap.set("n", "<space>ts", function() neotest.summary.show() end, { desc = "[test] show summary" })
+vim.keymap.set("n", "<space>to", function() neotest.output.open() end, { desc = "[test] show output" })
+vim.keymap.set("n", "<space>ts", function() neotest.summary.open() end, { desc = "[test] show summary" })
