2af41d3821556afb5acc76d3ed263abbb8712d46
Alan Oliveira <alan.oliveira89@gmail.com>
2022-10-05 22:38:16 +0900

    show recording macro on lualine and remove some hls

--- nvim/after/plugin/lualine.lua
+++ nvim/after/plugin/lualine.lua
@@ -1,5 +1,24 @@
 require("lualine").setup({
+  options = {
+    refresh = {
+      statusline = 500,
+    },
+  },
   sections = {
+    lualine_a = {
+      "mode",
+      {
+        color = { bg = "red" },
+        function()
+          local regrec = vim.fn.reg_recording()
+          if #regrec > 0 then
+            -- local icon = (os.time() % 2 == 0) and "◉" or " "
+            return "@" .. regrec
+          end
+          return ""
+        end,
+      },
+    },
     lualine_c = {
       {
         "filename",
--- nvim/appearance.vim
+++ nvim/appearance.vim
@@ -11,14 +11,5 @@ set listchars=tab:»-,trail:-,eol:↴,extends:»,precedes:«,nbsp:%,space:⋅
 
 highlight SpellBad guisp=red cterm=undercurl
 
-highlight QuickFixLine ctermfg=NONE guifg=NONE
-match ExtraWhitespace /\s\+\%#\@<!$/
-highlight ExtraWhitespace ctermbg=red guibg=red
-
 highlight! link MatchWord MatchParen
 highlight! link MatchWordCur MatchParenCur
-
-augroup _appearance_settings
-    autocmd!
-    autocmd FileType alpha highlight ExtraWhitespace NONE
-augroup end
