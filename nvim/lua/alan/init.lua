2b185544a4f5851c465171179fa42218e1b76f91
Alan Oliveira <alan.oliveira89@gmail.com>
2022-10-06 18:55:03 +0900

    Fix gitsigns shortcuts

--- nvim/lua/alan/init.lua
+++ nvim/lua/alan/init.lua
@@ -8,21 +8,6 @@ map({ "t", "n" }, "<C-l>", "<cmd> lua require('smart-splits').move_cursor_right(
 map("n", "<space>/", "<cmd> ToggleHLS<CR>", { desc = "toggle hls" })
 map("s", "<C-w>", "<BS>i", { desc = "delete word" }) -- used to delete suggestions on luasnip
 
--- Git
-map({ "n", "v" }, "<space>gs", "<cmd> :Gitsigns stage_hunk<CR>", { desc = "stage hunk" })
-map({ "n", "v" }, "<space>gr", ":Gitsigns reset_hunk<CR>", { desc = "reset hunk" })
-map("n", "<space>gS", require("gitsigns").stage_buffer, { desc = "stage buffer" })
-map("n", "<space>gu", require("gitsigns").undo_stage_hunk, { desc = "undo stage hunk" })
-map("n", "<space>gR", require("gitsigns").reset_buffer, { desc = "reset buffer" })
-map("n", "<space>gp", require("gitsigns").preview_hunk, { desc = "preview hunk" })
-map("n", "<space>gb", function() require("gitsigns").blame_line { full = true } end, { desc = "blame line" })
-map("n", "<space>gtb", require("gitsigns").toggle_current_line_blame, { desc = "toggle blame line" })
-map("n", "<space>gd", require("gitsigns").diffthis, { desc = "diff hunk" })
-map("n", "<space>gD", function() require("gitsigns").diffthis("~") end, { desc = "diff file" })
-map("n", "<space>gtd", require("gitsigns").toggle_deleted, { desc = "toggle deleted" })
-map("n", "]g", "&diff ? ']c' : '<cmd>lua require\"gitsigns\".next_hunk()<CR>'", { desc = "next git change", expr = true })
-map("n", "[g", "&diff ? '[c' : '<cmd>lua require\"gitsigns\".prev_hunk()<CR>'", { desc = "prev git change", expr = true })
-
 local alan = vim.api.nvim_create_augroup("alan", {})
 vim.api.nvim_create_autocmd("InsertEnter", { group = alan, command = "set norelativenumber" })
 vim.api.nvim_create_autocmd("InsertLeave", { group = alan, command = "set relativenumber" })
