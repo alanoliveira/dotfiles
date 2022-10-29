be6f079cbc1557c11b1b9d9fd27569ea81447539
Alan Oliveira <alan.oliveira89@gmail.com>
2022-10-29 11:14:39 +0900

    Remove zsh self-insert bindings

--- home/.config/zsh/zshrc.d/bindings.zsh
+++ home/.config/zsh/zshrc.d/bindings.zsh
@@ -1,3 +1,4 @@
+bindkey -v
 bindkey '^r'    fzf-select-history-widget
 bindkey '^t'    fzf-select-file-widget
 bindkey '^[c'   fzf-jump-to-z-dir-widget
@@ -5,3 +6,8 @@ bindkey '^xb'   fzf-select-git-branch-widget
 bindkey '^xk'   fzf-kill-process-widget
 bindkey '^f'    autosuggest-accept
 bindkey '^b'    util-open-on-browser
+
+for b in $(bindkey | grep self-insert | cut -d'"' -f2)
+do
+  bindkey -r "$b"
+done
