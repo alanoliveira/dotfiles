93dbea47233177382cfa4690b43ec3ad0163d211
Alan Oliveira <alan.oliveira89@gmail.com>
2022-11-29 00:00:25 +0900

    move direnv au

--- .config/nvim/lua/alan/commands.lua
+++ .config/nvim/lua/alan/commands.lua
@@ -5,9 +5,6 @@ vim.api.nvim_create_autocmd("InsertLeave",
   { group = alan, command = "if (&number || &relativenumber) | set rnu | endif" })
 vim.api.nvim_create_autocmd("TextYankPost",
   { group = alan, pattern = "*", command = "lua require('vim.highlight').on_yank()" })
-vim.api.nvim_create_autocmd("VimEnter", { group = alan, command = "DirenvExport" })
-vim.api.nvim_create_autocmd("DirChanged", { group = alan, command = "DirenvExport" })
-
 vim.api.nvim_create_user_command("TrimTrailingWhiteSpaces", "%s/\\s\\+$//e", {})
 vim.api.nvim_create_user_command("ClearSearch", "let @/ = ''", {})
 vim.api.nvim_create_user_command("ToggleHLS", "if (&hls && v:hlsearch) | noh | else | set hls | endif", {})
--- .config/nvim/lua/alan/config/direnv.lua
+++ .config/nvim/lua/alan/config/direnv.lua
@@ -1,1 +1,5 @@
 vim.g.direnv_auto = 0
+
+local group = vim.api.nvim_create_augroup("direnv", {});
+vim.api.nvim_create_autocmd("VimEnter", { group = group, command = "DirenvExport" })
+vim.api.nvim_create_autocmd("DirChanged", { group = group, command = "DirenvExport" })
