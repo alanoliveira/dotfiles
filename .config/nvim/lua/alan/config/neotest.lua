ccebf1e179d5122b383fa4bd5521a7a36725302b
Alan Oliveira <alan.oliveira89@gmail.com>
2022-11-29 22:19:56 +0900

    neotest config

--- .config/nvim/lua/alan/config/neotest.lua
+++ .config/nvim/lua/alan/config/neotest.lua
@@ -4,11 +4,13 @@ neotest.setup({
     require("neotest-go"),
     require("neotest-rspec"),
     require("neotest-plenary"),
+    require("neotest-rust"),
   },
 })
 
 vim.keymap.set("n", "<leader>tn", function() neotest.run.run() end, { desc = "[test] run nearest" })
 vim.keymap.set("n", "<leader>tf", function() neotest.run.run(vim.fn.expand("%")) end, { desc = "[test] run file" })
 vim.keymap.set("n", "<leader>tl", function() neotest.run.run_last() end, { desc = "[test] run last" })
+vim.keymap.set("n", "<leader>tm", function() neotest.run.run_marked() end, { desc = "[test] run marked" })
 vim.keymap.set("n", "<leader>to", function() neotest.output.open() end, { desc = "[test] show output" })
-vim.keymap.set("n", "<leader>ts", function() neotest.summary.open() end, { desc = "[test] show summary" })
+vim.keymap.set("n", "<leader>ts", function() neotest.summary.toggle() end, { desc = "[test] show summary" })
