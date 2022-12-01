b65ee2707018254fac0449484f143d4682ecf7ed
Alan Oliveira <alan.oliveira89@gmail.com>
2022-12-01 21:47:15 +0900

    test non mono NF

--- .config/tmux/scripts/tmux-pane-titler.sh
+++ .config/tmux/scripts/tmux-pane-titler.sh
@@ -3,9 +3,9 @@
 # $1 = pane_current_command
 # $2 = pane_current_path
 case "$1" in
-  $(basename $SHELL)) echo "$(basename $2)"
+  $(basename $SHELL)) echo " $(basename $2)"
   ;;
-  nvim) echo "$(basename $2)"
+  nvim) echo " $(basename $2)"
   ;;
   *) echo $1
   ;;
