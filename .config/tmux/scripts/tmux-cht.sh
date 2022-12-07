e367f17888ad17437c9b4ff0da8639d753718b45
Alan Oliveira <alan.oliveira89@gmail.com>
2022-12-07 22:13:27 +0900

    improve tmux-cht

--- .config/tmux/scripts/tmux-cht.sh
+++ .config/tmux/scripts/tmux-cht.sh
@@ -1,17 +1,11 @@
 #!/usr/bin/env bash
 
-complex=("rust" "golang" "ruby" "php" "nodejs" "tmux" "zsh" "lua" "bash" "css" "html")
-
 read -p "Enter Subject: " subject
 if [ -z $subject ]; then
   exit 0
 fi
 
 read -p "Enter Query: " query
-query=`echo $query | tr ' ' '+'`
+query=`echo $query | sed "s/^\w/\/\0/" | tr ' ' '+'`
 
-if [[ " ${complex[*]} " =~ " ${subject} " ]]; then
-  tmux new-window bash -c "echo \"curl -s cht.sh/$subject/$query/\" & curl -s cht.sh/$subject/$query | less -r"
-else
-  tmux new-window bash -c "curl -s cht.sh/$subject~$query | less -r"
-fi
+tmux new-window bash -c "curl -s cht.sh/$subject$query | less -r"
