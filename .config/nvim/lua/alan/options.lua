f65288497575134be29d20939bb979846d868eb3
Alan Oliveira <alan.oliveira89@gmail.com>
2022-12-10 18:17:33 +0900

    change scheme style

--- .config/nvim/lua/alan/options.lua
+++ .config/nvim/lua/alan/options.lua
@@ -40,6 +40,8 @@ vim.opt.undofile = true
 vim.g.mapleader = " "
 
 vim.cmd([[
+let g:sonokai_style = 'shusia'
+let g:sonokai_better_performance = 1
 colorscheme sonokai
 " highlight! SpellBad guisp=red cterm=undercurl
 highlight! link MatchWord MatchParen
