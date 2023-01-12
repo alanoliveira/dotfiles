b883663eae6daaf97972831095d24edc755344a0
Alan Oliveira <alan.oliveira89@gmail.com>
2023-01-12 21:12:51 +0900

    Improve hlsearch toggle

--- .config/nvim/lua/alan/customizations.lua
+++ .config/nvim/lua/alan/customizations.lua
@@ -7,8 +7,18 @@ vim.api.nvim_create_autocmd("TextYankPost",
   { group = alan, pattern = "*", command = "lua require('vim.highlight').on_yank()" })
 vim.api.nvim_create_user_command("TrimTrailingWhiteSpaces", "%s/\\s\\+$//e", {})
 vim.api.nvim_create_user_command("ClearSearch", "let @/ = ''", {})
-vim.api.nvim_create_user_command("ToggleHLS", "if (&hls && v:hlsearch) | noh | else | set hls | endif", {})
 vim.api.nvim_create_user_command("TermTab", "tabnew +term | set nobuflisted", {})
+
+vim.on_key(function(char)
+  if vim.fn.mode() == "n" then
+    local keys = { "n", "N", "*", "#", "<CR>", "/", "?" }
+    local new_hlsearch = vim.tbl_contains(keys, vim.fn.keytrans(char))
+
+    if vim.opt.hlsearch:get() ~= new_hlsearch then
+      vim.opt.hlsearch = new_hlsearch
+    end
+  end
+end, vim.api.nvim_create_namespace("toggle_hlsearch"))
 
 local map = vim.keymap.set
 
@@ -16,7 +26,6 @@ map({ "t", "n" }, "<C-h>", "<cmd> lua require('smart-splits').move_cursor_left()
 map({ "t", "n" }, "<C-j>", "<cmd> lua require('smart-splits').move_cursor_down()<CR>", { desc = "move to down split" })
 map({ "t", "n" }, "<C-k>", "<cmd> lua require('smart-splits').move_cursor_up()<CR>", { desc = "mode to up split" })
 map({ "t", "n" }, "<C-l>", "<cmd> lua require('smart-splits').move_cursor_right()<CR>", { desc = "move to right split" })
-map("n", "<leader>/", "<cmd> ToggleHLS<CR>", { desc = "toggle hls" })
 map("s", "<C-w>", "<BS>i", { desc = "delete word" }) -- used to delete suggestions on luasnip
 
 map("x", "<leader>p", [["_dp]], { desc = "p w/o change \" reg" })
