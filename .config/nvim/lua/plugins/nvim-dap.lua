a0ae2a2ca7c9aea176df80f7e77f32d8ef5755bb
Alan Oliveira <alan.oliveira89@gmail.com>
2023-02-04 12:21:26 +0900

    dap changes
    dap

--- .config/nvim/lua/plugins/nvim-dap.lua
+++ .config/nvim/lua/plugins/nvim-dap.lua
@@ -2,35 +2,49 @@ return {
   "mfussenegger/nvim-dap",
   keys = "<leader>d",
   dependencies = {
-    "rcarriga/nvim-dap-ui", -- debugger ui
     "nvim-telescope/telescope-dap.nvim", -- telescope integration
     "theHamsta/nvim-dap-virtual-text", -- show current variable values
+    "rcarriga/nvim-dap-ui", -- debugger ui
   },
   config = function()
     local dap = require("dap")
-    require("dapui").setup()
     require("nvim-dap-virtual-text").setup()
 
-    dap.adapters.lldb = {
-      type = "executable",
-      command = "lldb-vscode",
-      name = "lldb",
+    dap.adapters.codelldb = {
+      type = "server",
+      port = "${port}",
+      executable = {
+        command = "codelldb",
+        args = { "--port", "${port}" },
+      },
     }
 
     dap.configurations.zig = {
       {
         name = "Launch",
-        type = "lldb",
+        type = "codelldb",
         request = "launch",
         program = "${workspaceFolder}/zig-out/bin/${workspaceFolderBasename}",
         cwd = "${workspaceFolder}",
+        console = "internalConsole",
+      },
+    }
+
+    dap.configurations.c = {
+      {
+        name = "Launch",
+        type = "codelldb",
+        request = "launch",
+        program = "${workspaceFolder}/a.out",
+        cwd = "${workspaceFolder}",
+        console = "internalConsole",
       },
     }
 
     dap.configurations.rust = {
       {
         name = "Launch",
-        type = "lldb",
+        type = "codelldb",
         request = "launch",
         program = "${workspaceFolder}/target/debug/${workspaceFolderBasename}",
         cwd = "${workspaceFolder}",
@@ -40,29 +54,22 @@ return {
 
     local dap_widgets = require "dap.ui.widgets";
     local map = vim.keymap.set
-    map("n", "<leader>db", "<cmd>lua require'dap'.toggle_breakpoint()<CR>", { desc = "[dap] toggle breakpoint" })
-    map("n", "<leader>dB", "<cmd>lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>",
+    map("n", "<leader>db", dap.toggle_breakpoint, { desc = "[dap] toggle breakpoint" })
+    map("n", "<leader>dB", function() dap.set_breakpoint(vim.fn.input("Breakpoint condition: ")) end,
       { desc = "[dap] conditional breakpoint" })
-    map("n", "<leader>dh", "<cmd>lua require'dap.ui.widgets'.hover()<CR>", { desc = "[dap] hover" })
-    map("n", "<leader>dr", "<cmd>lua require'dap'.repl.toggle()<CR>", { desc = "[dap] toggle REPL" })
-    map("n", "<leader>dut", "<cmd>lua require'dapui'.toggle()<CR>", { desc = "[dap] toggle dapui" })
+    map("n", "<leader>dh", dap_widgets.hover, { desc = "[dap] hover" })
+    map("n", "<leader>dr", dap.repl.toggle, { desc = "[dap] toggle REPL" })
     map("n", "<leader>dus", function() dap_widgets.centered_float(dap_widgets.scopes) end, { desc = "[dap] ui scopes" })
     map("n", "<leader>duf", function() dap_widgets.centered_float(dap_widgets.frames) end, { desc = "[dap] ui frames" })
     map("n", "<leader>due", function() dap_widgets.centered_float(dap_widgets.expression) end,
       { desc = "[dap] ui expressions" })
     map("n", "<leader>dut", function() dap_widgets.centered_float(dap_widgets.threads) end, { desc = "[dap] ui threads" })
-    map("n", "<F5>", "<cmd>lua require'dap'.continue()<CR>", { desc = "[dap] continue" })
-    map("n", "<F10>", "<cmd>lua require'dap'.step_over()<CR>", { desc = "[dap] step over" })
-    map("n", "<F11>", "<cmd>lua require'dap'.step_into()<CR>", { desc = "[dap] step into" })
-    map("n", "<F12>", "<cmd> require'dap'.step_out()<CR>", { desc = "[dap] step out" })
+    map("n", "<leader>duu", "<cmd>lua require'dapui'.toggle()<CR>", { desc = "[dap] dapui toggle" })
+    map("n", "<leader>dc", function() dap.continue(); dap.repl.open() end, { desc = "[dap] continue" })
 
     local ok, telescope = pcall(require, "telescope")
     if ok then
       telescope.load_extension("dap")
     end
-
-
-
   end,
-
 }
