357adbf8cab11806ae958a6a23b95438228e0227
Alan Oliveira <alan.oliveira89@gmail.com>
2023-02-09 10:21:56 +0900

    improve tmux smart move

--- .config/tmux/scripts/tmux-smart-move-window.sh
+++ .config/tmux/scripts/tmux-smart-move-window.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+if [ $1 =~ '^[0-9]+$' ]; then
+  exit 0
+fi
+
 last_window=$(tmux lsw -F "#{window_index}" | tail -n1)
 dst=$(( $1 > $last_window ? $last_window : $1 ))
 tmux move-window -t 0
@@ -9,3 +13,4 @@ do
   tmux move-window -t 0 -s $dst -d
   tmux move-window -r
 done
+tmux display-message "window was moved to #{window_index}"
