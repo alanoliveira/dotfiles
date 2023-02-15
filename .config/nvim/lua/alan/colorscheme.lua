ca5994c6b266ea52dfa7767fab6d5f9937c67b1f
Alan Oliveira <alan.oliveira89@gmail.com>
2023-02-15 21:27:37 +0900

    change colorscheme

--- .config/nvim/lua/alan/colorscheme.lua
+++
@@ -1,4 +0,0 @@
-vim.cmd([[
-colorscheme github_dark
-highlight @text.literal ctermfg=14 gui=italic guifg=#aaf3bd " github_dark default text literal is hard to read
-]])
--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -15,12 +15,4 @@ return {
   -- language specifc stuffs
   { "slim-template/vim-slim", ft = "slim" },
   { "ziglang/zig.vim", ft = "zig" },
-
-  -- colorschemes
-  { "sainnhe/gruvbox-material", lazy = true },
-  { "sainnhe/sonokai", lazy = true },
-  { "rebelot/kanagawa.nvim", lazy = true },
-  { "gruvbox-community/gruvbox", lazy = true },
-  { "folke/tokyonight.nvim", lazy = true },
-  { "projekt0n/github-nvim-theme", lazy = true },
 }
