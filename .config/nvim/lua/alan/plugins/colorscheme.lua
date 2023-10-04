e02007157467faedbe397e8438a58f3672e0254b
Alan Oliveira <alan.oliveira89@gmail.com>
2023-10-04 19:52:48 +0900

    catppuccin latte

--- .config/nvim/lua/alan/plugins/colorscheme.lua
+++ .config/nvim/lua/alan/plugins/colorscheme.lua
@@ -1,6 +1,8 @@
 return {
-  "EdenEast/nightfox.nvim",
+  "catppuccin/nvim",
+  lazy = false,
+  priority = 1000,
   init = function()
-    vim.cmd("colorscheme nightfox")
+    vim.cmd("colorscheme catppuccin-latte")
   end,
 }
